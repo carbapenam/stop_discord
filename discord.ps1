84c06a075f5f9ab6c0d002e92aeacf561a7e73c7
carbapenam <3783158+carbapenam@users.noreply.github.com>
2019-02-09 02:11:18 -0600

    first commit

---
+++ discord.ps1
@@ -0,0 +1,26 @@
+# Configuration
+
+$lower_hour_limit = 23 # hh:00:00
+$upper_hour_limit = 23 # hh:59:59
+
+$lower_date_limit = Get-Date -Hour $lower_hour_limit -Minute 00 -Second 00
+$upper_date_limit = Get-Date -Hour $upper_hour_limit -Minute 59 -Second 59
+
+$current_date = Get-Date
+
+# Set Discord path here.
+
+$discord_path = ''
+$discord_exec_name = discord
+
+# Only turn on discord between the hours.
+
+if ($current_date -gt $lower_date_limit -and $current_date -lt $upper_date_limit) {$discord}
+
+# Check every minute if the time's passed.
+while(1)
+{
+   $current_date = Get-Date
+   if ($current_date -gt $upper_date_limit) {Stop-Process -name $discord_exec_name break;}
+   start-sleep -seconds 60
+}
